98f32045f6c29b724339c0d3401c710a3d98011c
Vic Chimenti <chimenti@seattleu.edu>
2020-02-23 09:48:27 -0800

    ud

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace Fibonacci
                     options.UseSqlServer(Configuration.GetConnectionString("FibonacciContext")));
 
 
-            services.AddMemoryCache();
+            //services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
